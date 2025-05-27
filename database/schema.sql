076c2487c0113994703f4f051d6bdae2115c765f
JoseVictor1998 <josevictor.dados@gmail.com>
2025-05-27 19:04:38 -0300

    Update schema.sql
    Adicionado tabelas de cobrança e de calculo de  juros  e parcelas.

--- database/schema.sql
+++ database/schema.sql
@@ -149,7 +149,6 @@ CREATE TABLE BankManagementSystem.dbo.Agencia (
 CREATE TABLE BankManagementSystem.dbo.Cargo (
 	CargoID int IDENTITY(1,1) NOT NULL,
 	Nome_Cargo nvarchar(25) COLLATE Latin1_General_CI_AS NOT NULL,
-	Nivel_Acessso nvarchar(10) COLLATE Latin1_General_CI_AS NOT NULL,
 	Nivel_Acesso int NOT NULL,
 	CONSTRAINT PK__Cargo__B4E665ED890B94E5 PRIMARY KEY (CargoID),
 	CONSTRAINT fk_Nivel_Acesso FOREIGN KEY (Nivel_Acesso) REFERENCES BankManagementSystem.dbo.Nivel_Acesso(Nivel_Acesso_ID)
@@ -262,3 +261,89 @@ CREATE TABLE BankManagementSystem.dbo.Historico_Conta (
 	Usuario_Responsavel nvarchar(100) COLLATE Latin1_General_CI_AS NULL,
 	CONSTRAINT PK__Historic__4A561D76F6D591A7 PRIMARY KEY (HistoricoID),
 	CONSTRAINT fk_ContaID FOREIGN KEY (ContaID) REFERENCES BankManagementSystem.dbo.Conta(ContaID)
+);
+
+
+ CREATE TABLE Configuracao_Juros(
+ Configuracao_ID INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+ Taxa_Juros_dia DECIMAL (3,3) NOT NULL CHECK (Taxa_Juros_dia >= 0),
+ Dias_Gratuitos INT NOT NULL CHECK (Dias_Gratuitos >=0),
+ Data_Inicio_Vigencia DATE NOT NULL,
+ Data_Fim_Vigencia DATE NULL
+ );
+ 
+ CREATE TABLE Cobranca(
+ Cobranca_ID INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+ Conta_ID INT NOT NULL,
+ Valor_Original DECIMAL (10,4) NOT NULL CHECK (Valor_Original >= 0),
+ Data_Vencimento DATE NOT NULL,
+ Data_Pagamento DATE NULL,
+ Valor_Juros_Aplicado DECIMAL (10,4) NOT NULL DEFAULT 0 CHECK (Valor_Juros_Aplicado >= 0 ),
+ Status_Cobranca NVARCHAR (25) NOT NULL DEFAULT 'Pendente',
+  Valor_Total AS (Valor_Original + Valor_Juros_Aplicado) PERSISTED,
+ CONSTRAINT fk_Conta_Cobranca FOREIGN KEY (Conta_ID) REFERENCES Conta(ContaID)
+ );
+ 
+ CREATE TABLE Historico_Cobranca(
+ Historico_ID INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+ Cobranca_ID INT NOT NULL,
+ Data_Alteracao DATETIME NOT NULL DEFAULT GETDATE(),
+ Tipo_Alteracao NVARCHAR (25) NOT NULL CHECK (Tipo_Alteracao IN ('Pendente','Pago','Cancelado','Atrasado','Atualizado')),
+ Observacao NVARCHAR (MAX),
+ CONSTRAINT fk_Cobranca_Historico FOREIGN KEY (Cobranca_ID) REFERENCES Cobranca(Cobranca_ID)
+ );
+ 
+ CREATE TABLE Negociacao_Divida(
+ Negociacao_ID INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+ Cliente_ID INT NOT NULL,
+ Data_Negociacao DATETIME NOT NULL DEFAULT GETDATE(),
+ Valor_Total_Negociacao DECIMAL (10,2) NOT NULL CHECK (Valor_Total_Negociacao > 0),
+ Numero_Parcelas INT NOT NULL CHECK (Numero_Parcelas >0),
+ Status NVARCHAR (25) NOT NULL CHECK (Status IN('Em andamento', 'Concluida','Cancelada')),
+ CONSTRAINT fk_Cliente_Negociacao FOREIGN KEY (Cliente_ID) REFERENCES Cliente(ClienteID)
+ );
+ 
+ 
+ CREATE TABLE Parcela_Negociacao(
+ Parcela_ID INT NOT NULL PRIMARY KEY IDENTITY (1,1), 
+ Negociacao_ID INT NOT NULL,
+ Valor_Parcela DECIMAL (10,2) NOT NULL CHECK (Valor_Parcela > 0),
+ Data_Vencimento DATE NOT NULL,
+ Data_Pagamento DATE NULL,
+ Status NVARCHAR (25) NOT NULL CHECK (Status IN('Paga','Em aberto','Vencida')),
+ CONSTRAINT fk_Parcela_Negociacao FOREIGN KEY (Negociacao_ID) REFERENCES Negociacao_Divida(Negociacao_ID)
+ );
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
